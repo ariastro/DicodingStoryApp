e3b97fe80052713deaa35ce81119c1bc8ed1e15b
Ari SWS <IDS.ARIS@xl.co.id>
2022-10-30 00:43:05 +0700

    Fix token interceptor

--- app/src/main/java/io/astronout/dicodingstoryapp/DicodingStoryApp.kt
+++ app/src/main/java/io/astronout/dicodingstoryapp/DicodingStoryApp.kt
@@ -2,7 +2,16 @@ package io.astronout.dicodingstoryapp
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class DicodingStoryApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
 }
--- app/src/main/java/io/astronout/dicodingstoryapp/data/source/remote/AuthInterceptor.kt
+++ app/src/main/java/io/astronout/dicodingstoryapp/data/source/remote/AuthInterceptor.kt
@@ -1,18 +1,22 @@
 package io.astronout.dicodingstoryapp.data.source.remote
 
 import io.astronout.dicodingstoryapp.data.source.local.LocalDataSource
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.Response
+import timber.log.Timber
 import javax.inject.Inject
 
 class AuthInterceptor @Inject constructor(private val localDataSource: LocalDataSource) : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val token = runBlocking {
+            delay(1000)
             localDataSource.getAuthToken().first()
         }
+        Timber.d("checkToken: $token")
         val originalRequest = chain.request()
         val requestBuilder = originalRequest.newBuilder()
             .header("Authorization", "Bearer $token")
